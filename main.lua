ded281317b8da747e3ae091fab2157b83e19a976
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-15 23:12:55 +0000

    Create basic player

---
+++ main.lua
@@ -0,0 +1,16 @@
+function love.load()
+  sprites = {}
+  sprites.coin_sheet = love.graphics.newImage('sprites/coin_sheet.png')
+  sprites.player_jump = love.graphics.newImage('sprites/player_jump.png')
+  sprites.player_stand = love.graphics.newImage('sprites/player_stand.png')
+
+  require('player')
+end
+
+function love.draw()
+  love.graphics.draw(sprites.player_stand, player.x, player.y)
+end
+
+function love.update(dt)
+
+end
