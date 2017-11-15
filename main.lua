96bbc11f6c7a8963b500b50688fd60f0a8950c3d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-15 23:18:13 +0000

    Implement physics for the player

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 function love.load()
+  myWorld = love.physics.newWorld(0, 100)
+
   sprites = {}
   sprites.coin_sheet = love.graphics.newImage('sprites/coin_sheet.png')
   sprites.player_jump = love.graphics.newImage('sprites/player_jump.png')
@@ -8,9 +10,9 @@ function love.load()
 end
 
 function love.draw()
-  love.graphics.draw(sprites.player_stand, player.x, player.y)
+  love.graphics.draw(sprites.player_stand, player.body:getX(), player.body:getY())
 end
 
 function love.update(dt)
-
+  myWorld:update(dt)
 end
