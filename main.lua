a24467374e1557c04cd572d4f6e8573b617e7c07
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-17 21:00:30 +0000

    Add jumping and movement

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
-  myWorld = love.physics.newWorld(0, 100)
+  myWorld = love.physics.newWorld(0, 500)
+  myWorld:setCallbacks(beginContact, endContact, preSolve, postSolve)
 
   sprites = {}
   sprites.coin_sheet = love.graphics.newImage('sprites/coin_sheet.png')
@@ -17,11 +18,26 @@ function love.draw()
     love.graphics.rectangle("fill", p.body:getX(), p.body:getY(), p.width, p.height)
   end
 
-  love.graphics.draw(sprites.player_stand, player.body:getX(), player.body:getY(), nil, nil, nil, sprites.player_stand:getWidth() / 2, sprites.player_stand:getHeight() / 2)
+  love.graphics.draw(sprites.player_stand, player.body:getX(), player.body:getY(), nil, player.direction, 1, sprites.player_stand:getWidth() / 2, sprites.player_stand:getHeight() / 2)
 end
 
 function love.update(dt)
   myWorld:update(dt)
+  playerUpdate(dt)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if (key == 'up' and player.grounded) then
+    player.body:applyLinearImpulse(0, -player.jumpForce)
+  end
+end
+
+function beginContact(a, b, coll)
+  player.grounded = true
+end
+
+function endContact(a, b, coll)
+  player.grounded = false
 end
 
 function spawnPlatform(x, y, width, height)
--- player.lua
+++ player.lua
@@ -2,3 +2,19 @@ player = {}
 player.body = love.physics.newBody(myWorld, 100, 100, "dynamic")
 player.shape = love.physics.newRectangleShape(66, 92)
 player.fixture = love.physics.newFixture(player.body, player.shape)
+player.speed = 200
+player.jumpForce = 2500
+player.grounded = false
+player.direction = 1
+
+function playerUpdate(dt)
+  if (love.keyboard.isDown("left")) then
+    player.body:setX(player.body:getX() - player.speed * dt)
+    player.direction = -1
+  end
+
+  if (love.keyboard.isDown("right")) then
+    player.body:setX(player.body:getX() + player.speed * dt)
+    player.direction = 1
+  end
+end
